b37ca58a9f4b103f337b9f72bb041690b820b8df
Louison <louison.druelle@manatal.com>
2021-10-28 12:48:56 +0700

    Add article details

--- src/views/articleDetails/ArticleDetails.js
+++ src/views/articleDetails/ArticleDetails.js
@@ -6,7 +6,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const ArticleDetails = () => {
   const articleId = window.location.pathname.slice(10);
-  const API_URL_TO_FETCH_SINGLE_ARTICLE = `${API_URL}${articleId}?api-key=${API_KEY}`;
+  const API_URL_TO_FETCH_SINGLE_ARTICLE = `${API_URL}${articleId}?show-fields=all&api-key=${API_KEY}`;
 
   const { data: article, error, isLoading } = useFetch(API_URL_TO_FETCH_SINGLE_ARTICLE);
 
@@ -15,7 +15,16 @@ const ArticleDetails = () => {
       { error && <div>{ error }</div> }
       { isLoading && <div>Loading...</div> }
       { article && 
-        <h2>{article.webTitle}</h2>
+        <div className="article-container">
+          <div className="article-content">
+            <h1>{article.fields.headline}</h1>
+            { article.fields.trailText && <div dangerouslySetInnerHTML={{__html: article.fields.trailText}} /> }
+            { article.fields.body && <div dangerouslySetInnerHTML={{__html: article.fields.body}} /> }
+          </div>
+          <div className="article-image">
+            { article.fields.main && <div dangerouslySetInnerHTML={{__html: article.fields.main}} /> }
+          </div>
+        </div>
       }
     </div>
   );
